fbbc7af47ba0f814e40769f36018ec4c898b12d9
noman1993 <chopolnoman07@gmail.com>
2025-05-04 14:16:48 +0600

    typescript object

---
+++ typescript/module1/src/data-types-2.ts
@@ -0,0 +1,16 @@
+// object
+
+let user: {
+    fName:string,
+    mdName: string,
+    lName: string,
+    isMarried?: true //optional type
+    company: "Jobbar Company" // type --> literal type (fixed value)\
+    // readonly company: string //alternative of literal types
+} = {
+    fName: "Rahim",
+    mdName: "Karim",
+    lName: "Jobbar",
+    company: "Jobbar Company"
+
+}
