33949caed0faa1d92c4489f543549198f3858b78
noman1993 <chopolnoman07@gmail.com>
2025-05-04 22:27:33 +0600

    type aliass

--- typescript/module1/src/type-alias.ts
+++ typescript/module1/src/type-alias.ts
@@ -25,9 +25,23 @@ const student2: Student = {
     address: "Khulna"
 }
 
+// type alias in string and boolean
+
 type UserName = string
 type IsAdmin = boolean
 
 const userName:UserName = "Rahim"
 const isAdmin: IsAdmin = true
+
+
+// type alias in function
+
+type Add = (num1:number,num2:number ) => number
+
+const add : Add = (num1, num2) => num1 + num2
+
+
+
+
+
 }
