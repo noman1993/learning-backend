74860e03262b843dffa5dddc2fc66e7d43eba890
noman1993 <chopolnoman07@gmail.com>
2025-05-04 09:44:51 +0600

    basic-data-type-1

---
+++ typescript/module1/src/data-types-1.ts
@@ -0,0 +1,28 @@
+//basic data-types
+
+//string
+
+let myName: string = "noman"
+
+//number
+let roll : number = 12
+
+//boolean
+
+let isAdmin : boolean = true
+
+//undefined
+let x: undefined = undefined
+
+//null 
+let y: null = null
+
+
+// Array
+
+let stdName: string[] = ['Rahim', 'Karim']
+let stdRoll : number[] = [1, 2]
+
+//tupple -> array --> order
+
+let student : [number, string, boolean] = [24, 'Rahim', true]
