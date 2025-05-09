02b37ca5772415433c8865f5a0745655d1d12840
noman1993 <chopolnoman07@gmail.com>
2025-05-09 09:23:43 +0600

    mod-2 type assertion

---
+++ typescript/module2/type-assertion-narrowing.ts
@@ -0,0 +1,43 @@
+
+{
+    //
+
+    //type assertion
+
+    let anything : any;
+
+    anything = "Next Level";
+
+    // (anything as string).
+
+
+
+
+    const kgToGrm = (value: string | number):string | number | undefined => {
+        if(typeof value ==='string'){
+            const convertedValue = parseInt(value)*1000
+            return `the converted value is ${convertedValue}`
+        }else if(typeof value === 'number'){
+            return value * 1000
+        }
+    }
+
+    const result1 = kgToGrm('100') as string
+    const result2 = kgToGrm(100) as number
+
+    
+    type CustomError = {
+        message: string
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log((error as CustomError).message);
+        
+    }
+
+
+
+    //
+}
