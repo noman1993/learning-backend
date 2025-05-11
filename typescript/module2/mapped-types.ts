acecf89fda7b62e266d6286850b5c10819f2b421
noman1993 <chopolnoman07@gmail.com>
2025-05-11 18:12:46 +0600

    mapped types

--- typescript/module2/mapped-types.ts
+++ typescript/module2/mapped-types.ts
@@ -6,5 +6,26 @@
     const arrayOfString: string[] = arrayOfNumbers.map((number)=> number.toString())
 
     console.log(arrayOfString);
+
+
+    type AreaNumber = {
+        height: number,
+        width: number
+    }
+
+    type AreaString = {
+        [key in keyof AreaNumber] : string
+    }
+
+
+    type areaElement<T> = {
+        [key in keyof T] : T[key]
+    }
+
+
+    const area1 : areaElement<{height:string, width: number}> = {
+        height: "100",
+        width: 50
+    }
     
 }
